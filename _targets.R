221c05bb16b75cf19757b5973c70ea3d04bbf3bc
gueye <gueye@latech.edu>
2026-07-14 19:14:47 -0500

    added cool comments

--- _targets.R
+++ _targets.R
@@ -8,16 +8,19 @@ lapply(X = r_paths, FUN = source) |>
   invisible()
 
 list(
+  # Scrape data
   targets::tar_target(
     poke_data,
     scrape_pokemon_data(url = "https://pokemondb.net/pokedex/all")
   ),
 
+  # Compute summary statistics
   targets::tar_target(
     attrs_by_type,
     compute_attrs_by_type(poke_data)
   ),
 
+  # Generate quarto report
   tarchetypes::tar_quarto(
     report,
     path = here::here("reporting/report.qmd"),
